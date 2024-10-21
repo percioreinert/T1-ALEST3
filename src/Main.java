955eb850463a83ff50d933ef03fa7ab190ef87cc
Pércio Reinert <percioreinert@gmail.com>
2024-10-21 20:23:42 -0300

    add tasks

--- src/Main.java
+++ src/Main.java
@@ -34,6 +34,8 @@ public class Main {
 
         // TODO: O rally termina quando alcançar a distância total.
 
+        // TODO: Retornar os pontos selecionados.
+
         return List.of(0);
     }
 
