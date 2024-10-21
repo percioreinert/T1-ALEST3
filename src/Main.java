e05e877a6b109cba7be659e8f79515b380cdf4a7
Pércio Reinert <percioreinert@gmail.com>
2024-10-21 20:02:38 -0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    /*
+    - Modelaremos a trilha do rally como um longo segmento de linha de comprimento 𝐿.
+    - Vocês conseguem viajar no máximo 𝑑 quilômetros por dia antes de anoitecer.
+    - Assumiremos que os pontos de parada estão localizados a distâncias 𝑥1, 𝑥2, … , 𝑥𝑛 do ponto de
+      partida.
+    - Assumiremos também que os seus amigos sempre estão corretos quando estimam se conseguem
+      ou não chegar ao próximo ponto de parada antes do anoitecer.
+    - Vamos considerar um conjunto de pontos de parada como válidos se a distância entre cada par
+      adjacente é no máximo 𝑑, e o primeiro ponto de parada está a no máximo uma distância 𝑑 do início
+      e o último ponto de parada está a uma distância no máximo 𝑑 do final da corrida. Portanto, um
+      conjunto de pontos de parada é valido se vocês conseguirem acampar nestes pontos e ainda
+      completar toda a trilha.
+    - Assumimos que o conjunto 𝑛 com todos os pontos de parada é valido.
+     */
+
+    // Distância L = quantos km? 100km por exemplo.
+    // Distância d = o máximo de km por dia. 10km por exemplo.
+    // Validar conjunto de entrada
+
+
+
+
+}
